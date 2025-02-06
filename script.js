a279a2e56b6ccfda096ea650cc4473c097bcf4aa
bentanoss <151925524+bentanoss@users.noreply.github.com>
2025-02-06 01:24:06 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,18 @@
 
 // Function to handle button click events
 function selectOption(option) {
-    // Check which option was clicked
     if (option === 'yes') {
-        // Flash rainbow colors
         flashRainbowColors(function() {
-            document.getElementById('question').style.display = 'none'; // Hide the question
-            displayCatHeart(); // Display the cat-heart.gif
+            window.location.href = 'second.html'; // Redirect to second page
         });
     } else if (option === 'no') {
-        // Change text on the "No" button to "You sure?"
         document.getElementById('no-button').innerText = 'You sure?'; 
-        // Increase font size of "Yes" button
         var yesButton = document.getElementById('yes-button');
-        var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
-        var newSize = parseFloat(currentFontSize) * 2; // Increase font size by  * 2px
+        var newSize = parseFloat(window.getComputedStyle(yesButton).fontSize) * 2;
         yesButton.style.fontSize = newSize + 'px';
-    } else {
-        // If neither "Yes" nor "No" was clicked, show an alert message
-        alert('Invalid option!');
     }
 }
+
 
 // Function to flash rainbow colors and then execute a callback function
 function flashRainbowColors(callback) {
